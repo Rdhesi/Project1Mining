0c47302cb12c8dd636512b813e21c3933c049169
Ravinder <Rdhesi@smith.edu>
2015-09-22 10:20:12 -0400

    First upload
    adding the first version of this project to Git

---
+++ Project 1 - Mining/Assets/spawnCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawnCube : MonoBehaviour {
+    public GameObject cube;
+    bool hasSpawned=false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+        if (Time.time>5&&hasSpawned==false)
+        {
+            Instantiate(cube, new Vector3(0, 0, 0), Quaternion.identity);
+            hasSpawned = true;
+        }
+	}
+}
